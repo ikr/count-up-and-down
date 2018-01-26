7547260697164be76611bc4bdb0db9a6f1308c0f
Ivan Krechetov <ikr@ikr.su>
2018-01-26 15:46:09 +0100

    Add the Error into the Model

--- src/Form.elm
+++ src/Form.elm
@@ -1,17 +1,27 @@
-module Form exposing (initialModel)
+module Form exposing (initialModel, Model)
 
 import Date exposing (Date)
 
 
 type alias Model =
+    ( Fields, Error )
+
+
+type alias Fields =
     { day : Int, month : Date.Month, year : Int, hour : Int, minute : Int }
+
+
+type alias Error =
+    Maybe String
 
 
 initialModel : Date -> Model
 initialModel defaultValue =
-    Model
+    ( Fields
         (Date.day defaultValue)
         (Date.month defaultValue)
         (Date.year defaultValue)
         (Date.hour defaultValue)
         (Date.minute defaultValue)
+    , Nothing
+    )
